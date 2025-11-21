1244b1f88c2194eef8d4f8b48f9aa5f963ba203f
chinmayee-giridhar <chinmayee.g14@gmail.com>
2025-11-21 16:39:15 +0530

    Update top200_clockgenes_merging.py

--- top200_clockgenes_merging.py
+++ top200_clockgenes_merging.py
@@ -1,27 +1,29 @@
 import pandas as pd
 
-#load files
+# load files
 top200 = pd.read_csv("./csvs/transposed_top200.csv")
 clock = pd.read_csv("./csvs/ClockExpressionLevels.csv")
 
-#renaming first column's name in top 200
+# rename columns
 top200.rename(columns={top200.columns[0]: "Gene"}, inplace=True)
-
-#renaming the first column in clock
 clock.rename(columns={"sample": "Gene"}, inplace=True)
 
-#ensuring same column names 
-common_samples = [c for c in top200.columns if c in clock.columns]
+# ensure same column names
+common_samples = [c for c in top200.columns if c in clock.columns and c != "Gene"]
 
-#reducing both to shared 
+# reduce both to shared columns
 top200 = top200[["Gene"] + common_samples]
 clock = clock[["Gene"] + common_samples]
 
-#merging
+# merge
 combined = pd.concat([top200, clock], ignore_index=True)
 
-#saving
-combined.to_csv("csvs/top200_clock_combined.csv", index=False)
+# remove duplicate column
+combined.drop_duplicates(subset=['Gene'], keep='first', inplace=True)
+
+# save
+combined.to_csv("csvs/top200+clock_combined.csv", index=False)
+
 
 
 
